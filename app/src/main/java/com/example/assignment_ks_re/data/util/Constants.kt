26fbc1ddce800d410f92a001839ed9169b00a973
Kim Kyungsik <ksiki1@naver.com>
2023-07-30 11:45:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/assignment_ks_re/data/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.assignment_ks_re.data.util
+
+object Constants {
+
+    const val BASE_URL = "https://dapi.kakao.com/"
+    const val API_KEY = "a9c9b825ab60749e299b375dc7b313e5"
+    const val SEARCH_BOOKS_TIME_DELAY = 100L
+
+}
